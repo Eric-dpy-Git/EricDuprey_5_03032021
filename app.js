5125435d7e166dba87f0b2f72b6321572fabb812
Eric <eric.dpy@gmail.com>
2021-03-11 00:06:11 +0100

    working addList toggle js + formatting index & product page

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const articles = document.querySelectorAll("article");
-articles[0].classList.add("hidden__class");
+const mainColor = document.getElementsByTagName("main");
+const articles = document.querySelectorAll("section");
 articles[1].classList.add("hidden__class");
+/* articles[1].classList.add("hidden__class");
 articles[2].classList.add("hidden__class");
 articles[3].classList.add("hidden__class");
-articles[4].classList.add("hidden__class");
+articles[4].classList.add("hidden__class"); */
+mainColor[0].classList.add("main__color");
 
 let listenBtn = document.getElementsByClassName("asideDivBtn__style");
 
 listenBtn[0].addEventListener("click", function (event) {
-  articles[0].classList.toggle("hidden__class");
   articles[1].classList.toggle("hidden__class");
+  /*  articles[1].classList.toggle("hidden__class");
   articles[2].classList.toggle("hidden__class");
   articles[3].classList.toggle("hidden__class");
-  articles[4].classList.toggle("hidden__class");
+  articles[4].classList.toggle("hidden__class"); */
+  mainColor[0].classList.toggle("main__color");
 });
