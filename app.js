cd1866f305c5c381bebef78522c9da75c248a893
Eric <eric.dpy@gmail.com>
2021-03-10 14:15:16 +0100

    First Js on DOM + Css & html work

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-let hiddenClass = document.querySelectorAll("article");
-for (let article of hiddenClass) {
-  article.classList.add("hidden__class");
-}
+const articles = document.querySelectorAll("article");
+articles[0].classList.add("hidden__class");
+articles[1].classList.add("hidden__class");
+articles[2].classList.add("hidden__class");
+articles[3].classList.add("hidden__class");
+articles[4].classList.add("hidden__class");
 
-let listenBtn = "asideDivBtn__style";
-listenBtn.addEventListener("click", function (event) {
-  article.classList.remove("hidden__class");
+let listenBtn = document.getElementsByClassName("asideDivBtn__style");
+
+listenBtn[0].addEventListener("click", function (event) {
+  articles[0].classList.toggle("hidden__class");
+  articles[1].classList.toggle("hidden__class");
+  articles[2].classList.toggle("hidden__class");
+  articles[3].classList.toggle("hidden__class");
+  articles[4].classList.toggle("hidden__class");
 });
