158974f257525834088999221fbb6f6c92ec45d5
Eric <eric.dpy@gmail.com>
2021-03-11 16:19:16 +0100

    rewrite Js & hidden__class improvement

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-const mainColor = document.getElementsByTagName("main");
-const articles = document.querySelectorAll("section");
-articles[1].classList.add("hidden__class");
-/* articles[1].classList.add("hidden__class");
-articles[2].classList.add("hidden__class");
-articles[3].classList.add("hidden__class");
-articles[4].classList.add("hidden__class"); */
-mainColor[0].classList.add("main__color");
+const mainColor = document.getElementsByTagName("main"); //constante pour l'élément main
+const section = document.querySelectorAll("section"); //constante pouir l'élément section
 
-let listenBtn = document.getElementsByClassName("asideDivBtn__style");
+let listenBtn = document.getElementsByClassName("asideDivBtn__style"); //variable pour l'élément bouton
 
+section[1].classList.add("hidden__class"); //ajoute la class
+mainColor[0].classList.add("main__color"); //ajoute la class
+
+//à l'écoute du click du bouton défini dans la variable
 listenBtn[0].addEventListener("click", function (event) {
-  articles[1].classList.toggle("hidden__class");
-  articles[0].classList.toggle("test");
-  /*  articles[1].classList.toggle("hidden__class");
-  articles[2].classList.toggle("hidden__class");
-  articles[3].classList.toggle("hidden__class");
-  articles[4].classList.toggle("hidden__class"); */
-  mainColor[0].classList.toggle("main__color");
+  section[1].classList.toggle("hidden__class"); //ajoute la class sur l'élément défini par une variable
+  mainColor[0].classList.toggle("main__color"); //ajoute la class sur l'élément défini par une variable
 });
