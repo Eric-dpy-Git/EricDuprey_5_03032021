05351c7fd961e31ada5b2bbd764cf6b6b105540c
Eric <eric.dpy@gmail.com>
2021-03-15 22:50:29 +0100

    find the way to display json in DOM

--- app.js
+++ app.js
@@ -14,15 +14,34 @@ listenBtn[0].addEventListener("click", function (event) {
 
 let url = "http://localhost:3000/api/cameras";
 
-let articles = document.getElementsByTagName("article");
 fetch(url).then((response) =>
   response.json().then((data) => {
     console.log(data);
-    let affichage = "<h2>";
-    for (let Camera of data) {
-      affichage += `<h2 id="h2-camera">${Camera.name}}</h2>`;
-    }
-    document.querySelector("#h2-camera").innerHTML = affichage;
-    /* document.querySelector("#camera").innerHTML = affichage; */
+    console.log(data.length);
+    console.log(data[0].name);
+    document.getElementById("h2-camera-0").textContent = data[0].name;
+    document.getElementById("img__1").src = data[2].imageUrl;
+    document.getElementById("description-1").textContent = data[0].description;
   })
 );
+
+/* fetch(url)
+  .then((res) => res.json())
+  .then((data) => (id = data[0].id));
+console.log(data[0].id); */
+
+/* let articles = document.getElementsByTagName("article"); */
+
+/* fetch(url).then((response) =>
+  response.json().then((data);
+  console.log(data) */
+/* => { */
+/*  console.log(data);
+    let affichage = "<h2>";
+    for (let camera of data) {
+      affichage += `<h2 id="h2-camera-0">${camera.name}</h2>`;
+    }
+    document.querySelector("#h2-camera-0").innerHTML = affichage; */
+/* document.querySelector("#camera").innerHTML = affichage; */
+/*  })
+); */
--- app2.js
+++ app2.js
@@ -12,16 +12,26 @@ fetch(url).then((response) =>
   })
 ); */
 
-fetch(url).then((response) =>
+/* fetch(url).then((response) =>
   response.json().then((data) => {
     console.log(data);
-    let affichage = "<ul>";
-    for (let Camera of data) {
-      affichage += `<div id="camera">${Camera.name}</div>`;
-      affichage += `<div id="camera-description">${Camera.description}</div>`;
-      affichage += `<img url="this" id="camera-img">${Camera.imageUrl}</img>`;
-    }
-    affichage += `</ul>`;
-    document.querySelector("#camera").innerHTML = affichage;
+    let affichage = "#camera-0"; */
+/*    for (let camera of data) { */
+/* affichage += `<div id="camera-0">${data[0].name}</div>`; */
+/*  affichage = `<div id="camera">${data[1].name}</div>`; */
+/*  affichage += `<div id="camera-description">${Camera.description}</div>`;
+      affichage += `<img url="" id="camera-img">${Camera.imageUrl}</img>`; */
+/*  }
+    document.querySelector("#camera-0").innerText = affichage; */
+/*  affichage += `</ul>`; */
+/* })
+);
+ */
+fetch(url).then((response) =>
+  response.json().then((data) => {
+    console.log(data.name);
+    console.log(data.length);
+    console.log(data[0].name);
+    document.getElementById("camera-0").textContent = data[0].name;
   })
 );
