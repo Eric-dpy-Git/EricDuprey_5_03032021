d8b90d5be9199cf5ea9d2008ed39484ab9b38e68
Eric <eric.dpy@gmail.com>
2021-03-21 01:46:50 +0100

    on way to find selctioned element in cart

--- product.js
+++ product.js
@@ -15,23 +15,52 @@ fetch(url).then((response) =>
     document.getElementById("product__price").innerHTML =
       //Divise price
       data.price / 100 + stringEuro;
+
+    //--------------------------
+    options = data.lenses;
+    console.log(options);
+
     //variable to show where to inject html
-    let show = "";
+    let show = `<select>`;
     //iteration loop of the array
     for (let i of data.lenses) {
       //in show where to inject html & insert the iteration --> the value can be a variable !
-      show += `<option value="${i}" class="option__product-choice" onchange="getSelectValue">${i}</option>`;
+      show += `<option value="${i}" class="option__product-choice">${i}</option>`;
     }
     //do the action to inject html with id emplacement
     document.getElementById("option__product").innerHTML = show;
+
+    /* for (let j of test1) {
+      test2 += `<option value="${j}">${j}</option>`;
+    }
+    console.log(test2);
+    const position = document.getElementById("option__product");
+    console.log(position);
+    position.innerHTML = test2; */
+    //-------------------------------------------------------------------------heeeerrree is
+    var e = document.getElementById("option__product");
+    var value = e.options[e.selectedIndex].value;
+    var text = e.options[e.selectedIndex].text;
+    //-------------------------------------------------------------------------heeeerrree is
+    console.log(value);
+    console.log(text);
+
+    const idForm = document.getElementById("option__product");
+
+    const choixForm = document.getElementsByClassName("option__product-choice");
+    console.log(choixForm);
+    const btnEnvoyerPanier = document.getElementById("btn__sent-cart");
+
+    btnEnvoyerPanier.addEventListener("click", (event) => {
+      event.preventDefault();
+    });
+    let otpionProduit = {
+      nonProduit: data.name,
+      prixProduit: data.price,
+    };
+    console.log(otpionProduit);
   })
 );
+
 //---------------------------------------add to cart---------------------------------
 //get form id
-let testing = document.getElementById("option__product");
-let result = document.getElementsByTagName("h2");
-
-testing.addEventListener,
-  function (event) {
-    console.log(selection.selectedIndex);
-  };
