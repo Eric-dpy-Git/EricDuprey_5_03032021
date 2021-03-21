2f509b79c52d7c73c625dd91e6fb83140d8bc300
Eric <eric.dpy@gmail.com>
2021-03-22 00:14:36 +0100

    local storage done

--- product.js
+++ product.js
@@ -1,66 +1,78 @@
 //retrieving query string in url
 const urlId = window.location.search;
-//to take off "?" and keep only id
+//to take off the "?" and keep only id
 const onlyId = urlId.slice(1);
 //creation of a server address variable + id
 url = `http://localhost:3000/api/cameras/${onlyId}`;
 fetch(url).then((response) =>
   response.json().then((data) => {
+    //inject product image
     document.getElementById("product__img").src = data.imageUrl;
+    //inject product name
     document.getElementById("product__title").innerText = data.name;
+    //inject product description
     document.getElementById("product__description").innerText =
       data.description;
-    //variable to get "€" after price
-    let stringEuro = " €";
-    document.getElementById("product__price").innerHTML =
-      //Divise price
-      data.price / 100 + stringEuro;
-
-    //--------------------------
-    options = data.lenses;
-    console.log(options);
-
+    //const number = prduct divised price
+    var number = data.price / 100;
+    // const format divised price in euro
+    const formatter = new Intl.NumberFormat("fr-FR", {
+      style: "currency",
+      currency: "EUR",
+    });
+    //inject divised price inside html
+    document.getElementById("product__price").innerHTML = formatter.format(
+      number
+    );
+    //------insert the choice of options in a drop-down menu-------------
     //variable to show where to inject html
-    let show = `<select>`;
+    let showOption = `<select>`; //???????????????question????????????????
     //iteration loop of the array
     for (let i of data.lenses) {
-      //in show where to inject html & insert the iteration --> the value can be a variable !
-      show += `<option value="${i}" class="option__product-choice">${i}</option>`;
+      //in showOption :  where to inject html & insert the iteration --> the value can be a variable !
+      showOption += `<option value="${i}" class="option__product-choice">${i}</option>`;
     }
     //do the action to inject html with id emplacement
-    document.getElementById("option__product").innerHTML = show;
-
-    /* for (let j of test1) {
-      test2 += `<option value="${j}">${j}</option>`;
-    }
-    console.log(test2);
-    const position = document.getElementById("option__product");
-    console.log(position);
-    position.innerHTML = test2; */
-    //-------------------------------------------------------------------------heeeerrree is
-    var e = document.getElementById("option__product");
-    var value = e.options[e.selectedIndex].value;
-    var text = e.options[e.selectedIndex].text;
-    //-------------------------------------------------------------------------heeeerrree is
-    console.log(value);
-    console.log(text);
-
-    const idForm = document.getElementById("option__product");
-
-    const choixForm = document.getElementsByClassName("option__product-choice");
-    console.log(choixForm);
-    const btnEnvoyerPanier = document.getElementById("btn__sent-cart");
-
-    btnEnvoyerPanier.addEventListener("click", (event) => {
+    document.getElementById("option__product").innerHTML = showOption;
+    //const for btn add to cart
+    const addToCart = document.getElementById("btn__addToCart");
+    //listen click add to cart btn
+    addToCart.addEventListener("click", (event) => {
       event.preventDefault();
+      //get the drop-down menu choice !!!inside eventlistener!!!
+      var e = document.getElementById("option__product");
+      /* var value = e.options[e.selectedIndex].value; */
+      var text = e.options[e.selectedIndex].text;
+      //list for local storage !!!inside eventlistener!!!
+      let productSet = {
+        productName: data.name,
+        productId: data._id,
+        productPrice: data.price,
+        choixLentille: text,
+      };
+      //---------------------------------------local storage---------------------------------
+      //parse methode for get items in local storage
+      let storedProduct = JSON.parse(localStorage.getItem("products"));
+      //alert message---------------------------not working
+      const alertMessage = () => {
+        if (window.confirm(`${data.name} option: ${text}`)) {
+          window.location.href("index.html");
+        } else window.location.href("index.html");
+      };
+      //---------------------------------------not working
+
+      //first : check if the local storage contains anything
+      if (storedProduct) {
+        storedProduct.push(productSet);
+        localStorage.setItem("products", JSON.stringify(storedProduct));
+        alertMessage();
+      } else {
+        storedProduct = [];
+        storedProduct.push(productSet);
+        localStorage.setItem("products", JSON.stringify(storedProduct));
+        console.log(storedProduct);
+        alertMessage();
+      }
     });
-    let otpionProduit = {
-      nonProduit: data.name,
-      prixProduit: data.price,
-    };
-    console.log(otpionProduit);
   })
 );
-
-//---------------------------------------add to cart---------------------------------
-//get form id
